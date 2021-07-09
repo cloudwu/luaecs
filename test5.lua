56ed66278e6c117a335084e51a2f7fc54b7185e7
Cloud Wu <cloudwu@gmail.com>
2021-07-09 17:16:05 +0800

    improve sync example

--- test5.lua
+++ test5.lua
@@ -7,6 +7,11 @@ w:register {
 	type = "int",
 }
 
+w:register {
+	name = "temp",
+	type = "int",
+}
+
 for i = 1, 10 do
 	w:new { a = i }
 end
@@ -14,10 +19,11 @@ end
 for v in w:select "a:in" do
 	if v.a %2 == 0 then
 		v.a = -v.a
-		w:sync("a:out", v)
+		v.temp = 42
+		w:sync("a:out temp:temp", v)
 	end
 end
 
-for v in w:select "a:in" do
-	print(v.a)
+for v in w:select "a:in temp?in" do
+	print(v.a, v.temp)
 end

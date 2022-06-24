a2db03ef340ebb4e7277d7412c53daa625d0f2e3
Cloud Wu <cloudwu@gmail.com>
2022-06-24 13:18:59 +0800

    Add test tag

--- test18.lua
+++ test18.lua
@@ -19,6 +19,10 @@ w:register {
 	name = "visible"
 }
 
+w:register {
+	name = "tag"
+}
+
 w:group_init "group"
 
 
@@ -26,12 +30,14 @@ w:new {
 	id = 0,
 	name = "zero",
 	group = 0,
+	tag = true,
 }
 
 w:new {
 	id = 1,
 	name = "one",
 	group = 0,
+	tag = true,
 }
 
 w:group_update()
@@ -39,16 +45,16 @@ w:update()
 
 w:group_enable("visible", 0)
 
-for v in w:select "visible id:in name:in" do
-	print("GROUP 0 : ",v.id, v.name)
+for v in w:select "visible id:in name:in tag?in" do
+	print("GROUP 0 : ",v.id, v.name, v.tag)
 	if v.id == 0 then
 		w:remove(v)	-- remove 0
 	end
 end
 
 for v in w:select "REMOVED" do
-	-- id 0 reborn, reset group to 1
-	w:clone(v, { group = 1 })
+	-- id 0 reborn, reset group to 1, and reset tag
+	w:clone(v, { group = 1, tag = false })
 end
 
 w:group_update()
@@ -56,8 +62,8 @@ w:update()
 
 w:group_enable("visible", 1)
 
-for v in w:select "visible id:in name:in" do
-	print("GROUP 1 : ", v.id, v.name)
+for v in w:select "visible id:in name:in tag?in" do
+	print("GROUP 1 : ", v.id, v.name, v.tag)
 end
 
 

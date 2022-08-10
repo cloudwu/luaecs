478bd72a465767d2954b9b2ca51231c94ad34a7d
Cloud Wu <cloudwu@gmail.com>
2022-08-10 18:43:09 +0800

    Add group test

--- test15.lua
+++ test15.lua
@@ -58,3 +58,71 @@ local g = w:group_get(0)
 for _, eid in ipairs(g) do
 	print("GROUP0", eid)
 end
+
+-------------------------
+local e = {}
+
+-- Random apply group
+for i = 1, 1000 do
+	local eid = w:new()
+	local group = math.random(2000, 2010)
+	e[eid] = group
+	w:group_add(group, eid)
+end
+
+-- Random remove
+for k,v in pairs(e) do
+	if math.random(1, 3) == 1 then
+		w:remove(k)
+		e[k] = nil
+	end
+end
+w:update()
+
+local function same_array(a, b)
+	local n = #a
+	assert(n == #b)
+	for i = 1, n do
+		assert(a[i] == b[i])
+	end
+end
+
+w:register {
+	name = "TEST"
+}
+
+local function insert_group(r, gid, ...)
+	if gid == nil then
+		return
+	end
+	for k,v in pairs(e) do
+		if v == gid then
+			table.insert(r, k)
+		end
+	end
+	insert_group(r, ...)
+end
+
+local function merge_group(...)
+	local r = {}
+	insert_group(r, ...)
+	table.sort(r)
+	return r
+end
+
+local function test(...)
+	local mg = merge_group(...)
+	w:group_enable("TEST", ...)
+	local r = {}
+	for v in w:select "TEST eid:in" do
+		table.insert(r, v.eid)
+	end
+	same_array(mg, r)
+end
+
+local function print_group(gid)
+	local g = w:group_get(gid)
+	print(table.concat(g, ","))
+end
+
+test(2000, 2001, 2005)

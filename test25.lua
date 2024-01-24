264b4c25d28af3b4ca8249e068107a2f993f3052
Cloud Wu <cloudwu@gmail.com>
2024-01-24 11:04:59 +0800

    test import

--- test25.lua
+++ test25.lua
@@ -15,6 +15,13 @@ local eid = w:new {
 	tag = true
 }
 
+local eid2 = w:new()
+
+w:import(eid2, {
+	string = "Hello2",
+	tag = true,
+})
+
 w:import(eid, {
 	string = "Hello",
 	tag = false,
@@ -22,3 +29,15 @@ w:import(eid, {
 
 assert(w:access(eid, "string") == "Hello")
 assert(w:access(eid, "tag") == false)
+
+assert(w:access(eid2, "string") == "Hello2")
+assert(w:access(eid2, "tag") == true)
+
+local s = {}
+
+for v in w:select "string:in" do
+	table.insert(s, v.string)
+end
+
+assert(s[1] == "Hello")
+assert(s[2] == "Hello2")
